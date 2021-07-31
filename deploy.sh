757392e5d993a6ad9dc966d3177bcaa35edec454
Rafael <rafael.a.rodriguez.m@gmail.com>
2021-07-31 10:00:01 -0400

    Change the deploy.sh to use SAM and lambda code basic

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,5 @@
 
 set -ue
 
-aws cloudformation package  --template-file template.yaml --s3-bucket "aws-sam-cli-managed-default-samclisourcebucket-nyt2w42xlyal" --output-template-file output-template.yaml
-aws cloudformation deploy  --template-file output-template.yaml --stack-name least-privilege-role --capabilities CAPABILITY_IAM
-
+sam deploy --no-confirm-changeset
 aws s3 cp template.yaml s3://apibucketleastprivilege/template.yaml
--- list_objects/app.py
+++ list_objects/app.py
@@ -16,16 +16,7 @@ def lambda_handler(event, context):
     """
     client_ssm = boto3.client('ssm')
     bucket_name = client_ssm.get_parameter(Name='/least_privilege/bucket_name')
-    client_s3 = boto3.client('s3')
-    list_object_resp = client_s3.list_objects(
-        Bucket=bucket_name['Parameter']['Value']
-    )
-    objects_names = [object['Key'] for object in list_object_resp['Contents']]
-    print(objects_names)
-    response = client_ssm.get_parameter(Name='/playground/accessanalyzer')
-    print(response['Parameter']['Value'])
-    print('{{"objects":{}}}'.format(objects_names))
     return {
         'statusCode': 200,
-        'body': json.dumps({"objects": objects_names})
+        'body': json.dumps({"bucketname": bucket_name['Parameter']['Value']})
     }
